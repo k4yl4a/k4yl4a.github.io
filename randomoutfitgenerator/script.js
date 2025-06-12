c325db2b0f8a17d338d08ce74025e44eb953858c
k4yl4a <kakgutie@ucsc.edu>
2025-06-12 00:41:27 +0000

    adding images-test

--- randomoutfitgenerator/script.js
+++ randomoutfitgenerator/script.js
@@ -1,25 +1,21 @@
 const tops = [
   'images/tops/top1.png',
   'images/tops/top2.png',
-  // add more
 ];
 
 const bottoms = [
   'images/bottoms/bottom1.png',
   'images/bottoms/bottom2.png',
-  // add more
 ];
 
 const shoes = [
   'images/shoes/shoe1.png',
   'images/shoes/shoe2.png',
-  // add more
 ];
 
 const accessories = [
   'images/accessories/acc1.png',
   'images/accessories/acc2.png',
-  // add more
 ];
 
 function getRandomItem(array) {
